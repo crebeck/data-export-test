3ac04686ffc68e504ccbe8ba1093e66602f7471a
crebeck <cbecker91@gmx.de>
2020-06-03 18:27:19 +0200

    Initial commit

---
+++ dictator/pages.py
@@ -0,0 +1,35 @@
+from ._builtin import Page, WaitPage
+from .models import Constants, DataExport
+
+# Import for data export
+from django.http import HttpResponse
+import vanilla
+import datetime
+import csv
+
+
+class Introduction(Page):
+    pass
+
+
+class Offer(Page):
+    form_model = 'group'
+    form_fields = ['kept']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 1
+
+
+class ResultsWaitPage(WaitPage):
+    after_all_players_arrive = 'set_payoffs'
+
+
+class Results(Page):
+    def vars_for_template(self):
+        return dict(offer=Constants.endowment - self.group.kept)
+
+    def before_next_page(self):
+        DataExport.objects.create(group=self.group, kept=self.group.kept)
+
+
+page_sequence = [Introduction, Offer, ResultsWaitPage, Results]
